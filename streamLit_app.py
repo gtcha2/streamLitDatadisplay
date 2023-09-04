c9c940cdc5aa4f80d9ef690c99360d4cc1fc3f02
gtcha2 <aron7628@gmail.com>
2023-09-04 15:25:22 -0500

    changing the tree data structure

--- streamLit_app.py
+++ streamLit_app.py
@@ -1,29 +1,26 @@
 import streamlit as st
 import json
-import pandas as pd
+from treelib import Node, Tree
 
-# Recursive function to flatten nested JSON objects
-def flatten_json(y):
-    """Flatten a nested JSON structure"""
-    out = {}
+# Recursive function to build a tree structure from nested JSON
+def build_tree(data, parent=None, tree=None):
+    if tree is None:
+        tree = Tree()
+    if isinstance(data, dict):
+        for key, value in data.items():
+            node_id = tree.get_node(parent).tag + f"_{key}"
+            tree.create_node(key, node_id, parent=parent, data=value)
+            build_tree(value, parent=node_id, tree=tree)
+    elif isinstance(data, list):
+        for i, item in enumerate(data):
+            node_id = tree.get_node(parent).tag + f"_{i}"
+            tree.create_node(f"[{i}]", node_id, parent=parent, data=item)
+            build_tree(item, parent=node_id, tree=tree)
 
-    def flatten(x, name=''):
-        if type(x) is dict:
-            for a in x:
-                flatten(x[a], name + a + '_')
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + '_')
-                i += 1
-        else:
-            out[name[:-1]] = x
-
-    flatten(y)
-    return out
+    return tree
 
 # Title of the Streamlit app
-st.title("JSON Data Viewer")
+st.title("JSON Data Explorer")
 
 # Create a file uploader to upload JSON data
 uploaded_file = st.file_uploader("Upload JSON File", type=["json"])
@@ -32,11 +29,12 @@ if uploaded_file is not None:
     # Read the uploaded JSON data
     data = json.load(uploaded_file)
 
-    # Flatten the JSON data
-    flat_data = flatten_json(data)
+    # Build a tree structure from the JSON data
+    root_node = "Root"
+    data_tree = build_tree(data, parent=root_node)
 
-    # Convert the flattened data to a DataFrame
-    df = pd.DataFrame([flat_data])
+    # Display the tree view of the JSON data
+    st.write("## Explore JSON Data")
+    st.write("Use the tree view below to explore the JSON data:")
+    st.write(data_tree.to_string())
 
-    # Display the DataFrame as a table
-    st.write(df)
