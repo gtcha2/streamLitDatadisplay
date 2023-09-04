2f8cec27ec1ea1120358089cc7b559dfc194a2ee
gtcha2 <aron7628@gmail.com>
2023-09-04 04:24:19 -0500

    new json viewer changes

--- streamLit_app.py
+++ streamLit_app.py
@@ -1,6 +1,28 @@
-# Title of the Streamlit app
 import streamlit as st
 import json
+import pandas as pd
+
+# Recursive function to flatten nested JSON objects
+def flatten_json(y):
+    """Flatten a nested JSON structure"""
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+# Title of the Streamlit app
 st.title("JSON Data Viewer")
 
 # Create a file uploader to upload JSON data
@@ -10,5 +32,11 @@ if uploaded_file is not None:
     # Read the uploaded JSON data
     data = json.load(uploaded_file)
 
-    # Display the JSON data in a code block
-    st.code(json.dumps(data, indent=2), language='json')
+    # Flatten the JSON data
+    flat_data = flatten_json(data)
+
+    # Convert the flattened data to a DataFrame
+    df = pd.DataFrame([flat_data])
+
+    # Display the DataFrame as a table
+    st.write(df)
