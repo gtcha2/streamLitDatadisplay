bccae2487869e4b30cf4f288c801717a60087e82
gtcha2 <aron7628@gmail.com>
2023-09-04 15:27:44 -0500

    fixing attr error 1

--- streamLit_app.py
+++ streamLit_app.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import json
-from treelib import Node, Tree
+from treelib import Tree
 
 # Recursive function to build a tree structure from nested JSON
 def build_tree(data, parent=None, tree=None):
     if tree is None:
         tree = Tree()
+        tree.create_node("Root", "root")
+    
     if isinstance(data, dict):
         for key, value in data.items():
-            node_id = tree.get_node(parent).tag + f"_{key}"
+            node_id = parent + f"_{key}"
             tree.create_node(key, node_id, parent=parent, data=value)
             build_tree(value, parent=node_id, tree=tree)
     elif isinstance(data, list):
         for i, item in enumerate(data):
-            node_id = tree.get_node(parent).tag + f"_{i}"
+            node_id = parent + f"_{i}"
             tree.create_node(f"[{i}]", node_id, parent=parent, data=item)
             build_tree(item, parent=node_id, tree=tree)
 
@@ -30,11 +32,10 @@ if uploaded_file is not None:
     data = json.load(uploaded_file)
 
     # Build a tree structure from the JSON data
-    root_node = "Root"
-    data_tree = build_tree(data, parent=root_node)
-
+    data_tree = build_tree(data, tree=Tree())
+    
     # Display the tree view of the JSON data
     st.write("## Explore JSON Data")
     st.write("Use the tree view below to explore the JSON data:")
-    st.write(data_tree.to_string())
+    st.write(data_tree.show(show_root=False))
 
