b0455df4f1855f87cf2d8505af48d2d21288cccb
gtcha2 <aron7628@gmail.com>
2023-09-06 09:32:34 -0500

    h

--- streamLit_app.py
+++ streamLit_app.py
@@ -21,7 +21,7 @@ def display_comment(comment, depth=0):
     indent = "----" * depth
     # st.write(f"depth:{depth}{indent}- {comment['body']}")
     
-    st.write(f"depth:{comment['depth']} id:{comment['id']}")
+    st.write(f"depth:{comment['depth']} {comment['id']}")
 # Display HTML conten
     st.markdown(comment['body_html'], unsafe_allow_html=True)
     if 'replies' in comment:
