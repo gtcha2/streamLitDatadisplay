29c05c4b9914391655b8b31e770ebdcb6758244f
Aaron Fanous <aron7268@gmail.com>
2024-06-28 20:49:08 -0700

    of course

--- streamLit_app.py
+++ streamLit_app.py
@@ -426,7 +426,7 @@ def doubleCheckLengths( userID, filter_option):
                     if (post.get("subreddit"),post.get('id'), str(post.get('comment_index'))) not in validSet:
                         validSet.add((post.get("subreddit"),post.get('id'), str(post.get('comment_index'))))
                         valid_posts.append(post)
-    st.session_state["test"]=len(validSet), len(filteredInformation["SampleID"].unique(),"new")
+    st.session_state["test"]=len(validSet), len(filteredInformation["SampleID"].unique()),"new"
     return
 def load_post_by_id(data, selected_subreddit, postID, commentID):
     if selected_subreddit in data:
